4db0092ae94807264e58ae2fd0349ca17a131c92
amuDev <12mcomco@gmail.com>
2021-12-19 12:34:05 -0600

    updated subgraph query, define strucutre for execution

--- src/query/index.ts
+++ src/query/index.ts
@@ -14,9 +14,10 @@ import {
 import { JSON, BigInt } from "@web3api/wasm-as";
 
 const predaDexAddress = "0x5101feD546FacccD309A77Ad755170f8fBf1E81D";
-const groupSwapAddress = "";
+// TODO: Update groupSwapAddress
+const groupSwapAddress = "0x5101feD546FacccD309A77Ad755170f8fBf1E81D";
 const ZERO = BigInt.fromString("0");
-const parts = "0";
+const parts = "1";
 const flags = "0";
 const destTokenEthPriceTimesGasPrice = "0";
 
@@ -32,15 +33,28 @@ export function checker(input: Input_checker): Gelato_CheckerResult {
     };
   }
 
+  // Get minimum gas required according to gasPrice passed by Gelato
+  const minGasUnitsForSwap = BigInt.fromString("100000"); // TODO: Validate this 100k number
+  const minWei = minGasUnitsForSwap.mul(gasPrice);
+  log(`Min wei: ${minWei}`);
+
   // Get orders from subgraph
+  // This query will return us only the groups that meet minimum requirements
   const groups = GraphNode_Query.querySubgraph({
     subgraphAuthor: "alphak3y",
     subgraphName: "predadex",
     query: `{
-      groupOrders(orderBy: groupGwei, orderDirection: asc) {
+      groupOrders(
+        orderBy: groupWei
+        orderDirection: desc
+        where: {
+          groupWei_gt: ${minWei}
+          groupAmount_gt: 0
+        }
+      ) {
         id
         groupAmount
-        groupGwei
+        groupWei
         fromToken
         destToken
       }
@@ -58,49 +72,36 @@ export function checker(input: Input_checker): Gelato_CheckerResult {
   const ordersArray = ordersArrayJSON.valueOf();
   // log(`ordersArray ${ordersArray.toString()}`);
 
-  // Get minimum gas required according to gasPrice passed by Gelato
-  const minGasUnitsForSwap = BigInt.fromString("100000");
-  const minGwei = minGasUnitsForSwap.mul(gasPrice);
-  log(`Min gwei: ${minGwei}`);
-
-  // let groupsToExecute;
+  let executeDataFromTokens = new JSON.Arr();
+  let executeDataDestTokens = new JSON.Arr();
+  let executeDataAmount = new JSON.Arr();
+  let executeDataQuoteAmount = new JSON.Arr();
 
   for (let i = 0; i < ordersArray.length; i++) {
     const ordersObj = <JSON.Obj>JSON.parse(ordersArray[i].stringify());
 
     const idJSONStr = ordersObj.getString("id");
-    const groupGweiJSONStr = ordersObj.getString("groupGwei");
+    const groupWeiJSONStr = ordersObj.getString("groupWei");
     const groupAmountJSONStr = ordersObj.getString("groupAmount");
     const fromTokenJSONStr = ordersObj.getString("fromToken");
     const destTokenJSONStr = ordersObj.getString("destToken");
     if (
       idJSONStr == null ||
-      groupGweiJSONStr == null ||
+      groupWeiJSONStr == null ||
       groupAmountJSONStr == null ||
       fromTokenJSONStr == null ||
       destTokenJSONStr == null
     )
       continue;
 
-    // groupAmount should not be zero
-    const groupAmount = BigInt.fromString(groupAmountJSONStr.valueOf());
-    if (groupAmount.lte(ZERO)) {
-      log(`[${idJSONStr.valueOf()}] : groupAmount <= zero`);
-      continue;
-    }
+    log(`id: ${idJSONStr}`);
+    log(`groupWei: ${groupWeiJSONStr}`);
 
-    // groupGwei should be more than gas for swap
-    const groupGwei = BigInt.fromString(groupGweiJSONStr.valueOf());
-    if (groupGwei.lt(minGwei)) {
-      log(`[${idJSONStr.valueOf()}] : groupGwei < minGwei`);
-      continue;
-    }
-
+    // This is failing probably because the contract is not deployed on ETH mainnet?
     const res = Ethereum_Query.callContractView({
       address: predaDexAddress,
       method:
         "function quoteAndDistribute(address fromToken, address destToken, uint256 amount, uint256 parts, uint256 flags, uint256 destTokenEthPriceTimesGasPrice) view returns(uint256 returnAmount, uint256[] memory distribution, uint256 gas)",
-      // TODO: how is this supposed to be formatted
       args: [
         fromTokenJSONStr.valueOf(),
         destTokenJSONStr.valueOf(),
@@ -113,20 +114,37 @@ export function checker(input: Input_checker): Gelato_CheckerResult {
     });
 
     log(`RES: ${res}`);
+
+    // It looks like res is a string type
+    // TODO: How do we convert the res string to the response args?
+
     // process res
-    // if (groupGwei < quotedGas * gasPrice) continue;
-    // groupsToExecute.push(currentGroup);
+    const weiRequired = BigInt.fromString(res).mul(gasPrice); // res.gas
+
+    const quoteAmount = new JSON.Str(res); // res.returnAmount
+
+    if (weiRequired <= BigInt.fromString(groupWeiJSONStr.toString())) {
+      executeDataFromTokens.push(fromTokenJSONStr);
+      executeDataDestTokens.push(destTokenJSONStr);
+      executeDataAmount.push(groupAmountJSONStr);
+      executeDataQuoteAmount.push(quoteAmount);
+    }
   }
 
   // Make execPayload
-  // const execPayload = Ethereum_Query.encodeFunction({
-  //   method: "function increaseCount(uint256)",
-  //   args: ["100"],
-  // });
+  const execPayload = Ethereum_Query.encodeFunction({
+    method: "function executeGroups(address[], address[], uint256[], uint256[])",
+    args: [
+      executeDataFromTokens.stringify.toString(),
+      executeDataDestTokens.stringify.toString(),
+      executeDataAmount.stringify.toString(),
+      executeDataQuoteAmount.stringify.toString()
+    ],
+  });
 
   const resolverData: Gelato_CheckerResult = {
     canExec: false,
-    execPayload: "",
+    execPayload: execPayload,
   };
 
   return resolverData;
